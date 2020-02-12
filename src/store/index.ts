0573bb35ddadda87a1c1462ec01dc6dec99ea0dd
r-sugi <r.sugimoto0615@gmail.com>
2020-02-12 23:33:40 +0900

    add: 8-3-3

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,8 +4,42 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    count: 0 as number | null,
+    name: null as string | null
+  },
+  getters: {
+    getName(state, getters) {
+      return state.name
+    },
+    greet(state, getters) {
+      return `My name is ${getters.getName.toUpperCase()}` // getters.getName may be null
+    }
+  },
+  mutations: {
+    setName(state, payload) {
+      state.name = payload // (property) name: string | null
+    },
+    increment(state) {
+      state.count++ // Error オブジェクトはnullである可能性があります
+    }
+  },
+  actions: {
+    asyncSetName(ctx, payload) {
+      ctx.commit('setName', { name: payload })
+      console.log(ctx.state.name)
+    },
+    asyncIncrement() {
+
+    },
+    async countup(ctx) {
+      while(true) {
+        await (() => new Promise(resolve => {
+          setTimeout(resolve, 1000)
+        }))()
+        ctx.dispatch('increment') // 存在しない Action Type
+      }
+    }
+  },
   modules: {}
 });
