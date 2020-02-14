6cbd11c4c021e376aa85dc1173eef7f30704d54d
r-sugi <r.sugimoto0615@gmail.com>
2020-02-15 03:40:51 +0900

    add: github module type

--- src/types/shims-vuex-impl.d.ts
+++ src/types/shims-vuex-impl.d.ts
@@ -1,12 +1,14 @@
 import "vuex";
-import * as Counter from "../store/counter/type";
+import * as Counter from "@/store/counter/type";
+import * as Github from "@/store/github/type";
 
 declare module "vuex" {
   type RootState = {
     counter: Counter.S;
+    github: Github.S;
   };
   // type RootGetters = Counter.RG & Todos.RG のように追加していく
   type RootGetters = Counter.RG; // &で連結していく
-  type RootMutations = Counter.RM; // &で連結していく
-  type RootActions = Counter.RA; // &で連結していく
+  type RootMutations = Counter.RM & Github.RM; // &で連結していく
+  type RootActions = Counter.RA & Github.RA; // &で連結していく
 }
